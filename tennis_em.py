9cd5aaf1e2a524f2396b6afbdfc8a12c54595a8b
Sam Duffield <s@mduffield.com>
2022-07-08 13:24:22 +0100

    add no draw

--- tennis_em.py
+++ tennis_em.py
@@ -1,4 +1,4 @@
-from jax import numpy as jnp, random
+from jax import numpy as jnp
 import matplotlib.pyplot as plt
 import pandas as pd
 
@@ -6,11 +6,11 @@ import models
 from smoothing import expectation_maximisation
 
 
-em_init_mean_and_var = jnp.array([0., 1.])
-em_init_tau = 1.
+em_init_mean_and_var = jnp.array([0., 10.])
+em_init_tau = jnp.sqrt(10.)
 em_init_s_and_epsilon = jnp.array([1., 0.])
 
-n_em_steps = 5
+n_em_steps = 20
 
 
 def consolidate_name_strings(name_series):
@@ -32,25 +32,20 @@ def clean_tennis_data(tennis_df_in, origin_date_str, name_to_id_dict):
     return tennis_df
 
 
-data_2020 = pd.read_csv('data/wta_2020.csv')
 data_2021 = pd.read_csv('data/wta_2021.csv')
-data_all = pd.concat([data_2020, data_2021])
 
-players_arr = pd.unique(pd.concat([consolidate_name_strings(data_all['Winner']),
-                                   consolidate_name_strings(data_all['Loser'])]))
+players_arr = pd.unique(pd.concat([consolidate_name_strings(data_2021['Winner']),
+                                   consolidate_name_strings(data_2021['Loser'])]))
 players_arr.sort()
 players_name_to_id_dict = {a: i for i, a in enumerate(players_arr)}
 players_id_to_name_dict = {i: a for i, a in enumerate(players_arr)}
-del data_all
 
-data_2020 = clean_tennis_data(data_2020, '2019-12-31', players_name_to_id_dict)
-data_2021 = clean_tennis_data(data_2021, '2019-12-31', players_name_to_id_dict)
+data_2021 = clean_tennis_data(data_2021, '2020-12-31', players_name_to_id_dict)
 
 train_match_times = jnp.array(data_2021['TimestampDays'])
 train_match_player_indices = jnp.array(data_2021[['WinnerID', 'LoserID']])
 train_match_results = jnp.ones_like(train_match_times)
 
-models.trueskill.init_time = 365
 # TrueSkill (EP)
 initial_params_ep, propagate_params_ep, update_params_ep = expectation_maximisation(models.trueskill.initiator,
                                                                                     models.trueskill.filter,
@@ -67,7 +62,6 @@ initial_params_ep, propagate_params_ep, update_params_ep = expectation_maximisat
 
 # TrueSkill (SMC)
 models.lsmc.n_particles = 1000
-models.trueskill.init_time = 365
 initial_params_smc, propagate_params_smc, update_params_smc = expectation_maximisation(models.lsmc.initiator,
                                                                                        models.lsmc.filter,
                                                                                        models.lsmc.smoother,
