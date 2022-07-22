ff59c411f7027be2e293648c310b62fae576a52a
Lorenzo Rimella <l.rimella@lancaster.ac.uk>
2022-07-22 15:41:17 +0100

    discrete skill filter

---
+++ models/discreteskill.py
@@ -0,0 +1,123 @@
+from typing import Tuple, Sequence, Callable
+
+from jax import numpy as jnp, random, vmap
+from jax.scipy.stats import norm
+# from jax.scipy.optimize import minimize
+from scipy.optimize import minimize
+
+from filtering import get_discrete_filter
+from smoothing import times_and_skills_by_player_to_by_match
+
+# skills.shape = (number of players, number of particles)
+# match_result in (0 for draw, 1 for p1 victory, 2 for p2 victory)
+# static_propagate_params = (tau,)
+#       tau = rate of dynamics
+# static_update_params = (s, epsilon)
+#       s = standard deviation of performance
+#       epsilon = draw margin
+
+init_time: float = 0.
+n_particles: int
+
+# Some kernel options we are going to use CTMC_kernel_reflected
+def CTMC_kernel_reflected(M, tau):
+    
+    skills_index = jnp.reshape(jnp.linspace(0, M-1, M), (M, 1))
+
+    # omegas  = np.pi*(skills_index)/(2*M)
+
+    omegas  = jnp.pi*(skills_index)/(2*M)
+    # omegas_lambda  = np.pi*(skills_index-1)/(2*M)
+    lambdas = jnp.cos(2*omegas) 
+
+    psi = jnp.sqrt(2/M)*jnp.cos(jnp.transpose(omegas)*(2*(skills_index+1)-1))
+
+    # psi[:,0] = psi[:,0]*jnp.sqrt(1/2) #assignment problem
+    psi = psi.at[:,0].set(psi[:,0]*jnp.sqrt(1/2))
+    
+    # def K_delta_t(delta_t_input):
+    
+    #     delta_t = jnp.reshape(delta_t_input, (len(delta_t_input), 1, 1))
+
+    #     time_lamb = (1-lambdas)*jnp.ones((len(delta_t_input), M, 1))
+    #     time_lamb = time_lamb*delta_t
+
+    #     time_eye = jnp.eye((M))*jnp.ones((len(delta_t_input), M, M))
+
+    #     expLambda = time_eye*jnp.exp(-tau*time_lamb)
+
+    #     K = jnp.einsum("tij,kj->tik", jnp.einsum("ij,tjk->tik", psi, expLambda), psi)
+
+    #     return jnp.abs(K)
+
+    def K_delta_t(delta_t):
+
+        time_lamb = (1-lambdas)*jnp.ones((M, 1))
+        time_lamb = time_lamb*delta_t
+
+        time_eye = jnp.eye((M))*jnp.ones((M, M))
+
+        expLambda = time_eye*jnp.exp(-tau*time_lamb)
+
+        K = jnp.einsum("ij,kj->ik", jnp.einsum("ij,jk->ik", psi, expLambda), psi)
+
+        return jnp.abs(K)
+
+    return K_delta_t
+
+
+# The emission matrix
+# think about draws
+def Phi_emission(s, epsilon, skills):
+    
+    skills_matrix  = jnp.reshape(jnp.linspace(0, skills-1, skills), (skills, 1))*jnp.ones((1, skills))
+    skills_diff    = (skills_matrix-jnp.transpose(skills_matrix))/s
+
+    phi_vic = jnp.reshape(   norm.cdf(skills_diff - epsilon/s), (skills, skills, 1))
+    phi_los = jnp.reshape(1- norm.cdf(skills_diff + epsilon/s), (skills, skills, 1))
+
+    return jnp.concatenate((1 - phi_vic- phi_los, phi_vic, phi_los), axis = 2)
+
+
+def initiator(num_players: int,
+              initial_distribution: jnp.ndarray,
+              random_key: jnp.ndarray) -> Tuple[jnp.ndarray, jnp.ndarray]:
+    M = (initial_distribution.shape[0])
+
+    return jnp.zeros(num_players) + init_time, jnp.ones((num_players, M))*initial_distribution
+
+def propagate(pi_tm1: jnp.ndarray,
+              time_interval: float,
+              tau: float,
+              random_key: jnp.ndarray) -> jnp.ndarray:
+
+    skills = pi_tm1.shape[0]
+    K_delta_t = CTMC_kernel_reflected(skills, tau)  
+
+    return jnp.einsum("j,jk->k", pi_tm1, K_delta_t(time_interval))
+
+def update(pi_t_tm1_p1: jnp.ndarray,
+           pi_t_tm1_p2: jnp.ndarray,
+           match_result: int,
+           s_epsilon: jnp.ndarray,
+           random_key: jnp.ndarray) -> Tuple[jnp.ndarray, jnp.ndarray, jnp.ndarray]:
+
+    s, epsilon = s_epsilon
+    skills = pi_t_tm1_p1.shape[0]
+    Phi = Phi_emission(s, epsilon, skills)
+
+    joint = jnp.reshape(pi_t_tm1_p1, (skills, 1, 1))*Phi*jnp.reshape(pi_t_tm1_p2, (1, skills, 1))
+
+    normalization  = jnp.sum(joint[:,:,match_result])
+
+    pl1 = jnp.sum(joint[:,:,match_result], axis = 1)/normalization
+    pl2 = jnp.sum(joint[:,:,match_result], axis = 0)/normalization
+
+    return pl1, pl2, jnp.sum(jnp.sum(joint, axis =0), axis =0)
+
+filter = get_discrete_filter(propagate, update)
+
+
+# smoother output array and matrix for the joint
+# smoothing is on 1 player as in filtering
+
--- models/lsmc.py
+++ models/lsmc.py
@@ -62,94 +62,94 @@ def update(skill_p1: jnp.ndarray,
 filter = get_random_filter(propagate, update)
 
 
-def smooth_single_sample(filter_skill_t: jnp.ndarray,
-                         time: float,
-                         smooth_skill_tplus1_single: jnp.ndarray,
-                         time_plus1: float,
-                         tau: float,
-                         random_key: jnp.ndarray) -> jnp.ndarray:
-    log_samp_probs = - jnp.square(smooth_skill_tplus1_single - filter_skill_t) / ((time_plus1 - time) * (tau ** 2))
-    samp_ind = random.categorical(random_key, log_samp_probs)
-    return filter_skill_t[samp_ind]
+# def smooth_single_sample(filter_skill_t: jnp.ndarray,
+#                          time: float,
+#                          smooth_skill_tplus1_single: jnp.ndarray,
+#                          time_plus1: float,
+#                          tau: float,
+#                          random_key: jnp.ndarray) -> jnp.ndarray:
+#     log_samp_probs = - jnp.square(smooth_skill_tplus1_single - filter_skill_t) / ((time_plus1 - time) * (tau ** 2))
+#     samp_ind = random.categorical(random_key, log_samp_probs)
+#     return filter_skill_t[samp_ind]
 
 
-def smoother(filter_skill_t: jnp.ndarray,
-             time: float,
-             smooth_skill_tplus1: jnp.ndarray,
-             time_plus1: float,
-             tau: float,
-             random_key: jnp.ndarray) -> Tuple[jnp.ndarray, None]:
-    rks = random.split(random_key, len(filter_skill_t))
-    return vmap(smooth_single_sample, in_axes=(None, None, 0, None, None, 0)) \
-               (filter_skill_t, time, smooth_skill_tplus1, time_plus1, tau, rks), None
+# def smoother(filter_skill_t: jnp.ndarray,
+#              time: float,
+#              smooth_skill_tplus1: jnp.ndarray,
+#              time_plus1: float,
+#              tau: float,
+#              random_key: jnp.ndarray) -> Tuple[jnp.ndarray, None]:
+#     rks = random.split(random_key, len(filter_skill_t))
+#     return vmap(smooth_single_sample, in_axes=(None, None, 0, None, None, 0)) \
+#                (filter_skill_t, time, smooth_skill_tplus1, time_plus1, tau, rks), None
 
 
-def get_sum_t1_diffs_single(times: jnp.ndarray,
-                            smoother_skills: jnp.ndarray) -> Tuple[int, float]:
-    time_diff = times[1:] - times[:-1]
-    smoother_diff2_div_time_diff = jnp.square(smoother_skills[1:] - smoother_skills[:-1]) / time_diff[..., jnp.newaxis]
+# def get_sum_t1_diffs_single(times: jnp.ndarray,
+#                             smoother_skills: jnp.ndarray) -> Tuple[int, float]:
+#     time_diff = times[1:] - times[:-1]
+#     smoother_diff2_div_time_diff = jnp.square(smoother_skills[1:] - smoother_skills[:-1]) / time_diff[..., jnp.newaxis]
 
-    return (~jnp.isnan(smoother_diff2_div_time_diff)).sum(), \
-           jnp.where(jnp.isnan(smoother_diff2_div_time_diff), 0, smoother_diff2_div_time_diff).sum()
-
-
-def maximiser_no_draw(times_by_player: Sequence,
-                      smoother_skills_and_extras_by_player: Sequence,
-                      match_player_indices_seq: jnp.ndarray,
-                      match_results: jnp.ndarray,
-                      initial_params: jnp.ndarray,
-                      propagate_params: jnp.ndarray,
-                      update_params: jnp.ndarray,
-                      i: int,
-                      random_key: jnp.ndarray) -> Tuple[jnp.ndarray, jnp.ndarray, jnp.ndarray]:
-    init_smoothing_skills = jnp.array([p[0][0] for p in smoother_skills_and_extras_by_player])
-    maxed_init_var = jnp.square(init_smoothing_skills - initial_params[0]).mean()
-    maxed_initial_params = initial_params.at[1].set(maxed_init_var)
-
-    smoother_skills_by_player = [ss for ss, _ in smoother_skills_and_extras_by_player]
-
-    num_diff_terms_and_diff_sums = jnp.array([get_sum_t1_diffs_single(t, s)
-                                              for t, s in zip(times_by_player, smoother_skills_by_player)])
-    maxed_tau = jnp.sqrt(num_diff_terms_and_diff_sums[:, 1].sum() / num_diff_terms_and_diff_sums[:, 0].sum())
-
-    return maxed_initial_params, maxed_tau, update_params
+#     return (~jnp.isnan(smoother_diff2_div_time_diff)).sum(), \
+#            jnp.where(jnp.isnan(smoother_diff2_div_time_diff), 0, smoother_diff2_div_time_diff).sum()
 
 
-def maximiser(times_by_player: Sequence,
-              smoother_skills_and_extras_by_player: Sequence,
-              match_player_indices_seq: jnp.ndarray,
-              match_results: jnp.ndarray,
-              initial_params: jnp.ndarray,
-              propagate_params: jnp.ndarray,
-              update_params: jnp.ndarray,
-              i: int,
-              random_key: jnp.ndarray) -> Tuple[jnp.ndarray, jnp.ndarray, jnp.ndarray]:
-    maxed_initial_params, maxed_tau, _ = maximiser_no_draw(times_by_player, smoother_skills_and_extras_by_player,
-                                                           match_player_indices_seq, match_results, initial_params,
-                                                           propagate_params, update_params, i, random_key)
+# def maximiser_no_draw(times_by_player: Sequence,
+#                       smoother_skills_and_extras_by_player: Sequence,
+#                       match_player_indices_seq: jnp.ndarray,
+#                       match_results: jnp.ndarray,
+#                       initial_params: jnp.ndarray,
+#                       propagate_params: jnp.ndarray,
+#                       update_params: jnp.ndarray,
+#                       i: int,
+#                       random_key: jnp.ndarray) -> Tuple[jnp.ndarray, jnp.ndarray, jnp.ndarray]:
+#     init_smoothing_skills = jnp.array([p[0][0] for p in smoother_skills_and_extras_by_player])
+#     maxed_init_var = jnp.square(init_smoothing_skills - initial_params[0]).mean()
+#     maxed_initial_params = initial_params.at[1].set(maxed_init_var)
 
-    smoother_skills_by_player = [ss for ss, _ in smoother_skills_and_extras_by_player]
+#     smoother_skills_by_player = [ss for ss, _ in smoother_skills_and_extras_by_player]
 
-    match_times, match_skills_p1, match_skills_p2 = times_and_skills_by_player_to_by_match(times_by_player,
-                                                                                           smoother_skills_by_player,
-                                                                                           match_player_indices_seq)
+#     num_diff_terms_and_diff_sums = jnp.array([get_sum_t1_diffs_single(t, s)
+#                                               for t, s in zip(times_by_player, smoother_skills_by_player)])
+#     maxed_tau = jnp.sqrt(num_diff_terms_and_diff_sums[:, 1].sum() / num_diff_terms_and_diff_sums[:, 0].sum())
 
-    def negative_expected_log_obs_dens(log_epsilon: jnp.ndarray) -> float:
-        def p_y_given_x(skill_p1: jnp.ndarray,
-                        skill_p2: jnp.ndarray,
-                        match_result: int) -> float:
-            s_and_eps = update_params.at[1].set(jnp.exp(log_epsilon[0]))
-            all_probs = predict(skill_p1, skill_p2, s_and_eps)[match_result]
-            all_probs = jnp.where(all_probs < 1e-20, 1e-20, all_probs)
-            return jnp.log(all_probs).mean()
+#     return maxed_initial_params, maxed_tau, update_params
 
-        return -vmap(p_y_given_x)(match_skills_p1, match_skills_p2, match_results).mean()
 
-    optim_res = minimize(negative_expected_log_obs_dens, jnp.log(update_params[-1:]), method='nelder-mead')
+# def maximiser(times_by_player: Sequence,
+#               smoother_skills_and_extras_by_player: Sequence,
+#               match_player_indices_seq: jnp.ndarray,
+#               match_results: jnp.ndarray,
+#               initial_params: jnp.ndarray,
+#               propagate_params: jnp.ndarray,
+#               update_params: jnp.ndarray,
+#               i: int,
+#               random_key: jnp.ndarray) -> Tuple[jnp.ndarray, jnp.ndarray, jnp.ndarray]:
+#     maxed_initial_params, maxed_tau, _ = maximiser_no_draw(times_by_player, smoother_skills_and_extras_by_player,
+#                                                            match_player_indices_seq, match_results, initial_params,
+#                                                            propagate_params, update_params, i, random_key)
 
-    assert optim_res.success, 'epsilon optimisation failed'
-    maxed_epsilon = jnp.exp(optim_res.x)[0]
-    maxed_s_and_epsilon = update_params.at[1].set(maxed_epsilon)
+#     smoother_skills_by_player = [ss for ss, _ in smoother_skills_and_extras_by_player]
 
-    return maxed_initial_params, maxed_tau, maxed_s_and_epsilon
+#     match_times, match_skills_p1, match_skills_p2 = times_and_skills_by_player_to_by_match(times_by_player,
+#                                                                                            smoother_skills_by_player,
+#                                                                                            match_player_indices_seq)
+
+#     def negative_expected_log_obs_dens(log_epsilon: jnp.ndarray) -> float:
+#         def p_y_given_x(skill_p1: jnp.ndarray,
+#                         skill_p2: jnp.ndarray,
+#                         match_result: int) -> float:
+#             s_and_eps = update_params.at[1].set(jnp.exp(log_epsilon[0]))
+#             all_probs = predict(skill_p1, skill_p2, s_and_eps)[match_result]
+#             all_probs = jnp.where(all_probs < 1e-20, 1e-20, all_probs)
+#             return jnp.log(all_probs).mean()
+
+#         return -vmap(p_y_given_x)(match_skills_p1, match_skills_p2, match_results).mean()
+
+#     optim_res = minimize(negative_expected_log_obs_dens, jnp.log(update_params[-1:]), method='nelder-mead')
+
+#     assert optim_res.success, 'epsilon optimisation failed'
+#     maxed_epsilon = jnp.exp(optim_res.x)[0]
+#     maxed_s_and_epsilon = update_params.at[1].set(maxed_epsilon)
+
+#     return maxed_initial_params, maxed_tau, maxed_s_and_epsilon
 
