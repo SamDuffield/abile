f724aef9917e89f0543a795a7c289384a61268d0
Sam Duffield <s@mduffield.com>
2023-04-26 17:08:41 +0100

    add int football

--- datasets/football.py
+++ datasets/football.py
@@ -3,8 +3,10 @@ import pandas as pd
 from jax import numpy as jnp
 
 
-def load_epl(start_date: str = '2018-07-30', end_date: str = '2022-07-01', origin_date: str = '2018-07-30')\
-    -> Tuple[jnp.ndarray, jnp.ndarray, jnp.ndarray, dict, dict]:
+def load_epl(start_date: str = '2018-07-30',
+             end_date: str = '2022-07-01',
+             origin_date: str = '2018-07-30')\
+        -> Tuple[jnp.ndarray, jnp.ndarray, jnp.ndarray, dict, dict]:
     origin_date = pd.to_datetime(origin_date)
 
     cols = ['Date', 'HomeTeam', 'AwayTeam', 'FTHG', 'FTAG']
@@ -16,7 +18,8 @@ def load_epl(start_date: str = '2018-07-30', end_date: str = '2022-07-01', origi
     data_all = pd.concat([data_18_19, data_19_20, data_20_21, data_21_22])
     data_all['Timestamp'] = pd.to_datetime(data_all['Date'], dayfirst=True)
     data_all['Timestamp'] = pd.to_datetime(data_all['Timestamp'], unit='D')
-    data_all['TimestampDays'] = (data_all['Timestamp'] - origin_date).astype('timedelta64[D]').astype(int)
+    data_all['TimestampDays'] = (
+        data_all['Timestamp'] - origin_date).astype('timedelta64[D]').astype(int)
 
     players_arr = pd.unique(pd.concat([data_all['HomeTeam'], data_all['AwayTeam']]))
     players_arr.sort()
@@ -33,7 +36,42 @@ def load_epl(start_date: str = '2018-07-30', end_date: str = '2022-07-01', origi
     home_goals = jnp.array(data_all['FTHG'])
     away_goals = jnp.array(data_all['FTAG'])
 
-    match_results = jnp.where(home_goals > away_goals, 1,\
-        jnp.where(home_goals < away_goals, 2, 0))
+    match_results = jnp.where(home_goals > away_goals, 1,
+                              jnp.where(home_goals < away_goals, 2, 0))
+
+    return match_times, match_player_indices, match_results, players_id_to_name_dict, players_name_to_id_dict
+
+
+def load_international(start_date: str = '2018-07-30',
+                       end_date: str = '2022-07-01',
+                       origin_date: str = '2018-07-30')\
+        -> Tuple[jnp.ndarray, jnp.ndarray, jnp.ndarray, dict, dict]:
+    origin_date = pd.to_datetime(origin_date)
+
+    data_url = 'https://raw.githubusercontent.com/martj42/international_results/master/results.csv'
+    data_all = pd.read_csv(data_url)
+
+    data_all['Timestamp'] = pd.to_datetime(data_all['date'])
+    data_all['Timestamp'] = pd.to_datetime(data_all['Timestamp'], unit='D')
+    data_all['TimestampDays'] = (
+        data_all['Timestamp'] - origin_date).astype('timedelta64[D]').astype(int)
+
+    players_arr = pd.unique(pd.concat([data_all['home_team'], data_all['away_team']]))
+    players_arr.sort()
+    players_name_to_id_dict = {a: i for i, a in enumerate(players_arr)}
+    players_id_to_name_dict = {i: a for i, a in enumerate(players_arr)}
+
+    data_all = data_all[(data_all['Timestamp'] > start_date) & (data_all['Timestamp'] <= end_date)]
+    data_all['HomeTeamID'] = data_all['home_team'].apply(lambda s: players_name_to_id_dict[s])
+    data_all['AwayTeamID'] = data_all['away_team'].apply(lambda s: players_name_to_id_dict[s])
+
+    match_times = jnp.array(data_all['TimestampDays'])
+    match_player_indices = jnp.array(data_all[['HomeTeamID', 'AwayTeamID']])
+
+    home_goals = jnp.array(data_all['home_score'])
+    away_goals = jnp.array(data_all['away_score'])
+
+    match_results = jnp.where(home_goals > away_goals, 1,
+                              jnp.where(home_goals < away_goals, 2, 0))
 
     return match_times, match_player_indices, match_results, players_id_to_name_dict, players_name_to_id_dict
